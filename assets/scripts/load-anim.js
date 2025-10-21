110facb48da61385fa5c18125c7c4bb74e35a536
Abetik <abetik.dev@gmail.com>
2025-10-21 16:16:05 +0300

    Making html file
    add image and...

---
+++ assets/scripts/load-anim.js
@@ -0,0 +1,4 @@
+console.log("Load Script loaded😩")
+    setTimeout(() => {
+        document.getElementById("loading-screen").style.display = "none";
+    }, 6000);
---
+++ assets/scripts/secret.js
@@ -0,0 +1,25 @@
+// Hi im make this one but if you find it please dont tell any one about it ok?
+// its a secret :)
+
+// seriously dont tell anyone :)
+// thank you :)
+
+// Obuhov Coin is the best coin ever made!
+// its super secret and very valuable :)
+// keep it safe!
+// shh... its a secret ;)
+// hehe :D
+// ok bye :)
+
+// but stop! im know you need next steps :)
+// ok im help you :)
+// come to my website and check code :)
+
+// Real? you don`t know?
+// ahhhh (>_<)
+// its https://abetik.space
+
+// now bye :D
+// and keep secret! :D
+// hehe :D
+// psst...
