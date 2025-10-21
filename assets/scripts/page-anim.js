abf91fbd720965d90104c469184822bbb07f27e3
Maks0101aps <maksim.lysak2010@gmail.com>
2025-10-21 17:31:34 +0300

    +page-anim

--- assets/scripts/page-anim.js
+++ assets/scripts/page-anim.js
@@ -5,12 +5,14 @@
 
     document.addEventListener('click', (e) => {
         const a = e.target.closest('a[href^="#"]');
-        if (!a) { 
-            return; 
+        if (!a) {
+            return;
         }
         const id = a.getAttribute('href').slice(1);
-        const target = document.getElementById(id); : null;
-        if(!target) return;
+        const target = document.getElementById(id);
+        if (!target) return;
         e.preventDefault();
+        target.scrollIntoView({ behavior: 'smooth', block: 'start' });
+        history.pushState(null, '', `#${id}`);
     });
 })();
